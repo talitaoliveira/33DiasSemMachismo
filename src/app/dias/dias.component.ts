6262ed1b906d1cb3afdf09209e9676f3b61e0241
Talita Silva de Oliveira <litaa.oliveira@gmail.com>
2017-06-05 12:33:26 -0300

    iniciando desenvolvimento do layout novo

--- src/app/dias/dias.component.ts
+++ src/app/dias/dias.component.ts
@@ -10,15 +10,26 @@ import { DiasService } from './dias.service';
 export class DiasComponent implements OnInit {
 
   hashTag: string = '';
-  dias;
-  cores;
-  cor;
-  nameDay: string = 'DIA';
+  nomeCampanha: string = '';
+  dias: any[];
+  imagensBgFrases: string[];
+  nameDay: string = '';
+
+  imagemBgInicio: string = '';
+
+  imagens33: string[];
 
   constructor(private diasService: DiasService) {
 
+    this.nameDay = 'dia';
     this.hashTag = '#33DiasSemMachismo';
+    this.nomeCampanha = '33DiasSemMachismo';
     this.dias = this.diasService.getDias();
+    this.imagemBgInicio = 'assets/images/bg-inicio.jpeg';
+
+    this.imagensBgFrases = this.diasService.getBackgrounds();
+
+    console.log(this.imagensBgFrases);
 
   }
 
@@ -27,11 +38,14 @@ export class DiasComponent implements OnInit {
     if (language === 'PT') {
       this.dias = this.diasService.getDias();
       this.hashTag = '#33DiasSemMachismo';
-      this.nameDay = 'DIA';
+      this.nomeCampanha = '33DiasSemMachismo';
+      this.nameDay = 'dia';
+
     } else {
       this.dias = this.diasService.getDiasIngles();
       this.hashTag = '#33DaysWithoutMachismo';
-      this.nameDay = 'DAY';
+      this.nomeCampanha = '33DaysWithoutMachismo';
+      this.nameDay = 'day';
     }
 
   }
